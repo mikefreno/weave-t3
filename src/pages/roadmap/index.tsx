c95542477b67177da88aa2a979b201808e852012
MikeFreno <michaelt.freno@gmail.com>
2023-03-03 05:25:00 -0500

    roadmap UI cleanup

--- src/pages/roadmap/index.tsx
+++ src/pages/roadmap/index.tsx
@@ -1,22 +1,33 @@
 import Footer from "@/src/components/home/Footer";
 import Navbar from "@/src/components/Navbar";
-import React, { useEffect, useRef } from "react";
+import ThemeContext from "@/src/components/ThemeContextProvider";
+import DoubleChevrons from "@/src/icons/DoubleChevrons";
+import React, { useContext, useEffect, useRef, useState } from "react";
 
 const RoadMap = () => {
   const switchRef = useRef<HTMLDivElement>(null);
+  const { isDarkTheme } = useContext(ThemeContext);
+  const [showingAside, setShowingAside] = useState(true);
+  const toggleNav = () => {
+    setShowingAside(!showingAside);
+  };
 
   return (
-    <div>
+    <div className="">
       <Navbar switchRef={switchRef} />
-      <div className="pt-20">
+      <div className="container pt-20">
         <aside
           id="side"
-          className="fixed h-screen w-52 border-r border-zinc-200 pt-2 pl-4"
+          className={`${
+            showingAside
+              ? "fixed h-screen w-[30vw] flex-col border-r border-zinc-200 pt-2 md:w-[20vw] md:px-4 xl:w-[15vw]"
+              : "hidden"
+          } `}
         >
-          <div className="text-xl font-semibold underline underline-offset-4">
+          <div className="pl-2 text-xl font-semibold underline underline-offset-4">
             Roadmap Overview
           </div>
-          <ul className="pl-4">
+          <ul>
             <li className="hvr-move-right">
               <a
                 href="#UI"
@@ -58,19 +69,42 @@ const RoadMap = () => {
               </a>
             </li>
           </ul>
+          <button className="flex justify-end align-bottom" onClick={toggleNav}>
+            <DoubleChevrons
+              height={36}
+              width={36}
+              stroke={isDarkTheme ? "#e4e4e7" : "#27272a"}
+              strokeWidth={1}
+            />
+          </button>
         </aside>
-        <div id="content" className="ml-52 flex-1 overflow-y-scroll pl-4">
+        <div className={showingAside ? "hidden" : ""}>
+          <button className="rotate-180" onClick={toggleNav}>
+            <DoubleChevrons
+              height={36}
+              width={36}
+              stroke={isDarkTheme ? "#e4e4e7" : "#27272a"}
+              strokeWidth={1}
+            />
+          </button>
+        </div>
+        <div
+          id="content"
+          className={`${
+            showingAside ? "ml-[30vw] md:ml-[20vw] xl:ml-[15vw]" : null
+          } flex flex-col overflow-y-scroll px-6 md:px-10 lg:px-16`}
+        >
           <div id="top" className="pb-16">
             <div className="text-3xl font-semibold tracking-wider">
               Weave Roadmap
             </div>
-            <p className="px-36 pt-12 tracking-wide">
+            <p className="pt-12 tracking-wide">
               This page is to outline the vision and progress of Weave. Broken
               down into the sections on the right, this roadmap will go over the
               various features planned and in development. Each feature will be
               labeled in the following way:{" "}
             </p>
-            <ul className="px-40">
+            <ul className="">
               <li className="flex">
                 <div className="my-auto h-3 w-3 rounded-full border border-zinc-400 bg-green-500 pt-1"></div>
                 <div className="pl-2"> indicates the feature is launched</div>
@@ -86,7 +120,7 @@ const RoadMap = () => {
                 <div className="pl-2">indicates the feature is planned</div>
               </li>
             </ul>
-            <p className="px-36 pt-2 tracking-wide">
+            <p className="pt-2 tracking-wide">
               Everything is subject to change, feature that are launched may be
               unlaunched, and features in development may be axed at any given
               time. Feel free to reach out on the github repo at{" "}
@@ -107,11 +141,11 @@ const RoadMap = () => {
             <div className="text-2xl font-semibold tracking-wider">
               User Interface
             </div>
-            <p className="px-36 pt-12 tracking-wide">
+            <p className="pt-12 tracking-wide">
               These features are mainly in reference to the way the app looks,
               and the way that you as a user interact with it
             </p>
-            <ul className="px-40">
+            <ul className="">
               <li className="flex">
                 <div className="my-auto h-3 w-3 rounded-full border border-zinc-400 bg-orange-500 pt-1"></div>{" "}
                 <div className="pl-2">
@@ -119,7 +153,7 @@ const RoadMap = () => {
                 </div>
               </li>
               <li className="flex">
-                <div className="my-auto h-3 w-3 rounded-full border border-zinc-400 bg-zinc-900 pt-1"></div>{" "}
+                <div className="my-auto h-3 w-3 rounded-full border border-zinc-400 bg-green-500 pt-1"></div>{" "}
                 <div className="pl-2">
                   Mobile experience improvements to front-page
                 </div>
@@ -156,10 +190,10 @@ const RoadMap = () => {
             <div className="text-2xl font-semibold tracking-wider">
               User / Account
             </div>
-            <p className="px-36 pt-12 tracking-wide">
+            <p className="pt-12 tracking-wide">
               These features regard your control over your account
             </p>
-            <ul className="px-40">
+            <ul className="">
               <li className="flex">
                 <div className="my-auto h-3 w-3 rounded-full border border-zinc-400 bg-orange-500 pt-1"></div>{" "}
                 <div className="pl-2">
@@ -181,15 +215,19 @@ const RoadMap = () => {
                 <div className="my-auto h-3 w-3 rounded-full border border-zinc-400 bg-zinc-900 pt-1"></div>{" "}
                 <div className="pl-2">Friends</div>
               </li>
+              <li className="flex">
+                <div className="my-auto h-3 w-3 rounded-full border border-zinc-400 bg-zinc-900 pt-1"></div>{" "}
+                <div className="pl-2">Direct Messaging</div>
+              </li>
             </ul>
           </div>
           <div id="server" className="absolute -mt-20"></div>
           <div>
             <div className="text-2xl font-semibold tracking-wider">Server</div>
-            <p className="px-36 pt-12 tracking-wide">
+            <p className="pt-12 tracking-wide">
               These features regard how servers function
             </p>
-            <ul className="px-40">
+            <ul className="">
               <li className="flex">
                 <div className="my-auto h-3 w-3 rounded-full border border-zinc-400 bg-orange-500 pt-1"></div>{" "}
                 <div className="pl-2">Message Link Preview</div>
@@ -233,10 +271,10 @@ const RoadMap = () => {
           <div id="bots" className="absolute -mt-20"></div>
           <div>
             <div className="text-2xl font-semibold tracking-wider">Bots</div>
-            <p className="px-36 pt-12 tracking-wide">
+            <p className="pt-12 tracking-wide">
               These features regard the usage of bots
             </p>
-            <ul className="px-40">
+            <ul className="">
               <li className="flex">
                 <div className="my-auto h-3 w-3 rounded-full border border-zinc-400 bg-zinc-900 pt-1"></div>{" "}
                 <div className="pl-2">Pre-made Bots</div>
@@ -252,11 +290,11 @@ const RoadMap = () => {
             <div className="text-2xl font-semibold tracking-wider">
               Miscellaneous
             </div>
-            <p className="px-36 pt-12 tracking-wide">
+            <p className="pt-12 tracking-wide">
               These features don&apos;t neatly fall into any of the previous
               categories
             </p>
-            <ul className="px-40">
+            <ul className="">
               <li className="flex">
                 <div className="my-auto h-3 w-3 rounded-full border border-zinc-400 bg-orange-500 pt-1"></div>{" "}
                 <div className="pl-2">Direct Messaging</div>
@@ -288,9 +326,13 @@ const RoadMap = () => {
             </ul>
           </div>
         </div>
-        <div className="ml-52 mb-4 mt-12 border-b border-zinc-800 dark:border-zinc-300" />
-        <Footer />
       </div>
+      <div
+        className={`${
+          showingAside ? "ml-[30vw] md:ml-[20vw] xl:ml-[15vw]" : null
+        } mb-4 mt-12 border-b border-zinc-800 dark:border-zinc-300`}
+      />
+      <Footer />
     </div>
   );
 };
