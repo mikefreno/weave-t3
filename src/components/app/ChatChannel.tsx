973f82f84a11c1e3a1f46fb848bb7a1456c171f4
MikeFreno <michaelt.freno@gmail.com>
2023-04-11 20:56:32 -0400

    trying a mobile fix

--- src/components/app/ChatChannel.tsx
+++ src/components/app/ChatChannel.tsx
@@ -111,7 +111,7 @@ const ChannelMain = (props: ChannelMainProps) => {
   const OtherCommentsClass =
     "bg-zinc-200 shadow-lg dark:bg-zinc-800 dark:shadow-zinc-700 rounded-2xl py-5 px-6";
 
-  const chatWindowHight = inputDivRef.current?.clientHeight;
+  const chatWindowHeight = inputDivRef.current?.clientHeight;
 
   return (
     <>
@@ -189,7 +189,7 @@ const ChannelMain = (props: ChannelMainProps) => {
           <div
             className={`fixed bottom-0 ${fullscreen ? "w-screen" : "w-full"}`}
           >
-            <button
+            {/* <button
               className="absolute right-0 z-50 -mt-12 md:hidden"
               onClick={paddingToggle}
             >
@@ -212,7 +212,7 @@ const ChannelMain = (props: ChannelMainProps) => {
                   />
                 </div>
               )}
-            </button>
+            </button> */}
             <div
               ref={inputDivRef}
               className={`${
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,7 @@ export default function Document() {
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <body className="">
         <Main />
