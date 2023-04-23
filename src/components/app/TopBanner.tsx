d347fb564aad1b4d4476cca8b218170a958b8cfd
MikeFreno <michaelt.freno@gmail.com>
2023-04-23 19:38:21 -0400

    banner ui fix in safari

--- src/components/app/TopBanner.tsx
+++ src/components/app/TopBanner.tsx
@@ -1,15 +1,21 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Server_Channel } from "@prisma/client";
 import HeadphonesIcon from "@/src/icons/HeadphonesIcon";
 import CommentsIcon from "@/src/icons/CommentsIcon";
 import ThemeContext from "../ThemeContextProvider";
 
 export default function TopBanner(props: {
-  currentChannel: Server_Channel;
+  selectedChannel: Server_Channel;
   fullscreen: boolean;
 }) {
   const { isDarkTheme } = useContext(ThemeContext);
-  const { currentChannel, fullscreen } = props;
+  const { selectedChannel, fullscreen } = props;
+
+  const [channelName, setChannelName] = useState(selectedChannel.name);
+
+  useEffect(() => {
+    setChannelName(selectedChannel.name);
+  }, [selectedChannel]);
 
   return (
     <div
@@ -18,7 +24,7 @@ export default function TopBanner(props: {
       } bg-purple-400 dark:bg-zinc-700`}
     >
       <div className="flex items-center pl-6 pt-3 text-xl underline underline-offset-8">
-        {currentChannel.type == "voice" ? (
+        {selectedChannel.type == "voice" ? (
           <HeadphonesIcon
             height={36}
             width={36}
@@ -32,7 +38,7 @@ export default function TopBanner(props: {
             strokeWidth={1}
           />
         )}
-        <div className="mx-6">{currentChannel.name}</div>
+        <div className="z-[1000] mx-6">{channelName}</div>
       </div>
     </div>
   );
