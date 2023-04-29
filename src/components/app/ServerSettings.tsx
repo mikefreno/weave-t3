4d84eab2c8747f0449b069daf7981c683cdd1b6f
MikeFreno <michaelt.freno@gmail.com>
2023-04-28 20:45:33 -0400

    server user mgmt finished first implementation

--- src/components/app/ServerSettings.tsx
+++ src/components/app/ServerSettings.tsx
@@ -1,4 +1,5 @@
 import BackArrow from "@/src/icons/BackArrow";
+import CircleSlashIcon from "@/src/icons/CircleSlashIcon";
 import HandRaiseIcon from "@/src/icons/HandRaiseIcon";
 import { api } from "@/src/utils/api";
 import { Tooltip } from "@nextui-org/react";
@@ -26,6 +27,8 @@ export default function ServerSettings(props: ServerSettingsProps) {
       })
     | null
   >(null);
+  const [width, setWidth] = useState<number>(window.innerWidth);
+  const smallScreenBoolean = width < 768;
 
   const serverDataQuery = api.server.getPrivilegeBasedServerData.useQuery({
     serverID: props.serverID,
@@ -42,6 +45,14 @@ export default function ServerSettings(props: ServerSettingsProps) {
       setPrivilegeLevelData(null);
     };
   }, [serverDataQuery]);
+
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   const promoteMemberToAdmin = async (promoteeID: string) => {
     if (props.serverID) {
@@ -67,99 +78,249 @@ export default function ServerSettings(props: ServerSettingsProps) {
     if (props.serverID) {
     }
   };
+  const banMember = async (memberID: string) => {
+    if (props.serverID) {
+    }
+  };
 
-  const adminSettingsOptions = () => {
-    if (props.privilegeLevel === "owner" || props.privilegeLevel === "admin") {
-      return (
-        <div className="w-5/6 rounded-lg bg-zinc-50 px-8 py-6 shadow-lg dark:bg-zinc-600">
-          <div className="pb-8 text-2xl tracking-widest underline underline-offset-2">User Management</div>
-          <div className="rule-around py-2 text-center text-xl">Admin</div>
-          <ul>
-            <div className="grid grid-cols-4">
-              <div className="underline underline-offset-4">Name</div>
-              <div className="underline underline-offset-4">Pseudonym</div>
-              <div className="underline underline-offset-4">Email</div>
-              <div className="underline underline-offset-4">Management Options</div>
+  const userManagementMobileScreenFormat = () => {
+    return (
+      <div>
+        <div className="rule-around py-2 text-center text-xl">Admin</div>
+        {privilegeLevelData?.admin?.length === 0 ? (
+          <div className="py-2 text-center italic">
+            No admin, you can promote a member to help with server administration
+          </div>
+        ) : null}
+        {privilegeLevelData?.admin?.map((admin) => (
+          <div className="my-4 rounded-md bg-purple-50 px-2 py-1 shadow-md dark:bg-zinc-700" key={admin.id}>
+            <div className="flex justify-between px-4 pb-2">
+              <div className="italic underline underline-offset-4">Name</div>
+              <div className="italic underline underline-offset-4">Pseudonym</div>
             </div>
-            {privilegeLevelData?.admin?.map((admin) => (
-              <div className="grid grid-cols-4 py-2 pl-4" key={admin.id}>
-                <div className="my-auto">
-                  {admin.admin.name ? admin.admin.name : <span className="italic">null</span>}
-                </div>
-                <div className="my-auto">
-                  {admin.admin.pseudonym ? admin.admin.pseudonym : <span className="italic">null</span>}
-                </div>
-                <div className="my-auto">
-                  {admin.admin.email === admin.admin.id ? "User Deleted" : admin.admin.email}
-                </div>
-                <div className="my-auto">
-                  {props.privilegeLevel === "owner" ? (
-                    <div>
-                      <Tooltip content={"Demote to member"}>
-                        <button
-                          className="rounded-md bg-red-500 p-2 hover:bg-red-600 active:bg-red-700"
-                          onClick={() => demoteAdminToMember(admin.adminId)}
-                        >
-                          <div className="-rotate-90">
-                            <BackArrow height={12} width={12} stroke={"white"} strokeWidth={1.5} />
-                          </div>
-                        </button>
-                      </Tooltip>
-                    </div>
-                  ) : null}
-                </div>
+            <div className="flex justify-between px-4 pb-2">
+              <div className="my-auto">
+                {admin.admin.name ? admin.admin.name : <span className="italic">null</span>}
               </div>
-            ))}
-            {privilegeLevelData?.admin?.length === 0 ? (
-              <div className="py-2 italic">No admin, you can promote a member to help with server administration</div>
-            ) : null}
-          </ul>
-          <div className="rule-around py-2 text-center text-xl">Members</div>
-          <ul>
-            <div className="grid grid-cols-4">
-              <div className="underline underline-offset-4">Name</div>
-              <div className="underline underline-offset-4">Pseudonym</div>
-              <div className="underline underline-offset-4">Email</div>
-              <div className="underline underline-offset-4">Management Options</div>
+              <div className="my-auto">
+                {admin.admin.pseudonym ? admin.admin.pseudonym : <span className="italic">null</span>}
+              </div>
             </div>
-            {privilegeLevelData?.members?.map((member) => (
-              <div className="grid grid-cols-4 py-2 pl-4" key={member.id}>
-                <div className="my-auto">
-                  {member.member.name ? member.member.name : <span className="italic">null</span>}
-                </div>
-                <div className="my-auto">
-                  {member.member.pseudonym ? member.member.pseudonym : <span className="italic">null</span>}
-                </div>
-                <div className="my-auto">{member.member.email}</div>
-                <div className="my-auto flex justify-between">
+            <div className="flex justify-between px-4 pb-2">
+              <div className="italic underline underline-offset-4">Email</div>
+              <div className="italic underline underline-offset-4">Management</div>
+            </div>
+            <div className="flex justify-between px-4 pb-2">
+              <div className="my-auto">{admin.admin.email === admin.admin.id ? "User Deleted" : admin.admin.email}</div>
+              <div className="my-auto">
+                {props.privilegeLevel === "owner" ? (
                   <div>
-                    <Tooltip content={"Promote to admin"}>
+                    <Tooltip content={"Demote to member"}>
                       <button
-                        className="rounded-md bg-purple-500 p-2 hover:bg-purple-600 active:bg-purple-700"
-                        onClick={() => promoteMemberToAdmin(member.memberId)}
+                        className="rounded-md bg-red-500 p-2 hover:bg-red-600 active:bg-red-700"
+                        onClick={() => demoteAdminToMember(admin.adminId)}
                       >
-                        <div className="rotate-90">
+                        <div className="-rotate-90">
                           <BackArrow height={12} width={12} stroke={"white"} strokeWidth={1.5} />
                         </div>
                       </button>
                     </Tooltip>
                   </div>
+                ) : null}
+              </div>
+            </div>
+          </div>
+        ))}
+        <div className="rule-around py-2 text-center text-xl">Members</div>
+        {privilegeLevelData?.members?.length === 0 ? (
+          <div className="py-2 text-center italic">No members yet!</div>
+        ) : null}
+        {privilegeLevelData?.members?.map((member) => (
+          <div className="my-4 rounded-md bg-purple-50 px-2 py-1 shadow-md dark:bg-zinc-700" key={member.id}>
+            <div className="flex justify-between px-4 pb-2">
+              <div className="italic underline underline-offset-4">Name</div>
+              <div className="italic underline underline-offset-4">Pseudonym</div>
+            </div>
+            <div className="flex justify-between px-4 pb-2">
+              <div className="my-auto">
+                {member.member.name ? member.member.name : <span className="italic">null</span>}
+              </div>
+              <div className="my-auto">
+                {member.member.pseudonym ? member.member.pseudonym : <span className="italic">null</span>}
+              </div>
+            </div>
+            <div className="flex justify-between px-4 pb-2">
+              <div className="italic underline underline-offset-4">Email</div>
+              <div className="italic underline underline-offset-4">Management</div>
+            </div>
+            <div className="flex justify-between px-4 pb-2">
+              <div className="my-auto">
+                {member.member.email === member.member.id ? "User Deleted" : member.member.email}
+              </div>
+              <div className="my-auto flex w-2/5 justify-between">
+                <div>
+                  <Tooltip content={"Promote to admin"}>
+                    <button
+                      className="rounded-md bg-purple-500 p-2 hover:bg-purple-600 active:bg-purple-700"
+                      onClick={() => promoteMemberToAdmin(member.memberId)}
+                    >
+                      <div className="rotate-90">
+                        <BackArrow height={12} width={12} stroke={"white"} strokeWidth={1.5} />
+                      </div>
+                    </button>
+                  </Tooltip>
+                </div>
+                <div>
+                  <Tooltip content={"Suspend member"}>
+                    <button
+                      className="rounded-md bg-orange-500 p-2 hover:bg-orange-600 active:bg-orange-700"
+                      onClick={() => suspendMember(member.memberId)}
+                    >
+                      <div className="">
+                        <HandRaiseIcon height={12} width={12} stroke={"white"} strokeWidth={1.5} />
+                      </div>
+                    </button>
+                  </Tooltip>
+                </div>
+                <div>
+                  <Tooltip content={"Ban member"}>
+                    <button
+                      className="rounded-md bg-red-500 p-2 hover:bg-red-600 active:bg-red-700"
+                      onClick={() => banMember(member.memberId)}
+                    >
+                      <div className="rotate-180">
+                        <CircleSlashIcon height={12} width={12} stroke={"white"} strokeWidth={1.5} />
+                      </div>
+                    </button>
+                  </Tooltip>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  const userManagementDesktopFormat = () => {
+    return (
+      <>
+        <div className="rule-around py-2 text-center text-xl">Admin</div>
+        {privilegeLevelData?.admin?.length !== 0 ? (
+          <div className="grid grid-cols-4 pb-2 pl-4">
+            <div className="underline underline-offset-4">Name</div>
+            <div className="underline underline-offset-4">Pseudonym</div>
+            <div className="underline underline-offset-4">Email</div>
+            <div className="underline underline-offset-4">Management</div>
+          </div>
+        ) : (
+          <div className="py-2 text-center italic">
+            No admin, you can promote a member to help with server administration
+          </div>
+        )}
+        {privilegeLevelData?.admin?.map((admin) => (
+          <div className="my-2 rounded-md bg-purple-50 px-2 py-1 shadow-md dark:bg-zinc-700" key={admin.id}>
+            <div className="grid grid-cols-2 py-2 pl-4 md:grid-cols-4">
+              <div className="my-auto">
+                {admin.admin.name ? admin.admin.name : <span className="italic">null</span>}
+              </div>
+              <div className="my-auto">
+                {admin.admin.pseudonym ? admin.admin.pseudonym : <span className="italic">null</span>}
+              </div>
+              <div className="my-auto">{admin.admin.email === admin.admin.id ? "User Deleted" : admin.admin.email}</div>
+              <div className="my-auto">
+                {props.privilegeLevel === "owner" ? (
                   <div>
-                    <Tooltip content={"Suspend member"}>
+                    <Tooltip content={"Demote to member"}>
                       <button
-                        className="rounded-md bg-purple-500 p-2 hover:bg-purple-600 active:bg-purple-700"
-                        onClick={() => suspendMember(member.memberId)}
+                        className="rounded-md bg-red-500 p-2 hover:bg-red-600 active:bg-red-700"
+                        onClick={() => demoteAdminToMember(admin.adminId)}
                       >
-                        <div className="rotate-180">
-                          <HandRaiseIcon height={12} width={12} stroke={"white"} strokeWidth={1.5} />
+                        <div className="-rotate-90">
+                          <BackArrow height={12} width={12} stroke={"white"} strokeWidth={1.5} />
                         </div>
                       </button>
                     </Tooltip>
                   </div>
+                ) : null}
+              </div>
+            </div>
+          </div>
+        ))}
+        <div className="rule-around py-2 text-center text-xl">Members</div>
+        {privilegeLevelData?.members?.length !== 0 ? (
+          <div className="grid grid-cols-4 pb-2 pl-4">
+            <div className="underline underline-offset-4">Name</div>
+            <div className="underline underline-offset-4">Pseudonym</div>
+            <div className="underline underline-offset-4">Email</div>
+            <div className="underline underline-offset-4">Management</div>
+          </div>
+        ) : (
+          <div className="py-2 text-center italic">No members yet!</div>
+        )}
+        {privilegeLevelData?.members?.map((member) => (
+          <div className="my-2 rounded-md bg-purple-50 px-2 py-1 shadow-md dark:bg-zinc-700" key={member.id}>
+            <div className="grid grid-cols-4 py-2 pl-4">
+              <div className="my-auto">
+                {member.member.name ? member.member.name : <span className="italic">null</span>}
+              </div>
+              <div className="my-auto">
+                {member.member.pseudonym ? member.member.pseudonym : <span className="italic">null</span>}
+              </div>
+              <div className="my-auto">
+                {member.member.email === member.member.id ? "User Deleted" : member.member.email}
+              </div>
+              <div className="my-auto flex w-5/6 justify-between">
+                <div>
+                  <Tooltip content={"Promote to admin"}>
+                    <button
+                      className="rounded-md bg-purple-500 p-2 hover:bg-purple-600 active:bg-purple-700"
+                      onClick={() => promoteMemberToAdmin(member.memberId)}
+                    >
+                      <div className="rotate-90">
+                        <BackArrow height={12} width={12} stroke={"white"} strokeWidth={1.5} />
+                      </div>
+                    </button>
+                  </Tooltip>
+                </div>
+                <div>
+                  <Tooltip content={"Suspend member"}>
+                    <button
+                      className="rounded-md bg-orange-500 p-2 hover:bg-orange-600 active:bg-orange-700"
+                      onClick={() => suspendMember(member.memberId)}
+                    >
+                      <div className="">
+                        <HandRaiseIcon height={12} width={12} stroke={"white"} strokeWidth={1.5} />
+                      </div>
+                    </button>
+                  </Tooltip>
+                </div>
+                <div>
+                  <Tooltip content={"Ban member"}>
+                    <button
+                      className="rounded-md bg-red-500 p-2 hover:bg-red-600 active:bg-red-700"
+                      onClick={() => banMember(member.memberId)}
+                    >
+                      <div className="rotate-180">
+                        <CircleSlashIcon height={12} width={12} stroke={"white"} strokeWidth={1.5} />
+                      </div>
+                    </button>
+                  </Tooltip>
                 </div>
               </div>
-            ))}
-          </ul>
+            </div>
+          </div>
+        ))}
+      </>
+    );
+  };
+
+  const adminSettingsOptions = () => {
+    if (props.privilegeLevel === "owner" || props.privilegeLevel === "admin") {
+      return (
+        <div className="w-full rounded-lg bg-zinc-50 px-4 py-6 shadow-lg dark:bg-zinc-600 md:w-5/6 md:px-8">
+          <div className="pb-8 text-2xl tracking-widest underline underline-offset-2">User Management</div>
+          {smallScreenBoolean ? userManagementMobileScreenFormat() : userManagementDesktopFormat()}
         </div>
       );
     }
@@ -171,14 +332,14 @@ export default function ServerSettings(props: ServerSettingsProps) {
   };
 
   return (
-    <div className="select-text px-8 py-6">
+    <div className="max-h-screen w-full select-text overflow-y-scroll p-2 md:px-8 md:py-6">
       <div className="text-center text-2xl tracking-widest">{privilegeLevelData?.name}</div>
       <div className="py-4 text-center text-xl tracking-wider">Settings</div>
       <div className="">
         {adminSettingsOptions()}
         {ownerSettingsOptions()}
       </div>
-      <div className="mt-8 w-5/6 rounded-lg bg-red-600 px-8 py-6 shadow-lg">
+      <div className="mt-8 w-full rounded-lg bg-red-600 px-8 py-6 shadow-lg md:w-5/6">
         <div className="py-2 text-2xl underline underline-offset-2">Danger Zone</div>
         {props.privilegeLevel !== "owner" ? (
           <button className="flex underline-offset-4 hover:underline">
--- src/components/home/Parallax_2.tsx
+++ src/components/home/Parallax_2.tsx
@@ -12,13 +12,15 @@ const Parallax_2 = (props: { isDarkTheme: boolean }) => {
   useEffect(() => {
     const handleScroll = () => {
       const offset = window.pageYOffset;
-      Array.from(parallaxRef.current!.children).map((layer: Element, index) => {
-        let speed = parseFloat(layer.getAttribute("data-speed")!);
-        if (index === 0 || index === 2) {
-          speed = speed * -1;
-        }
-        (layer as HTMLElement).style.transform = `translateY(${offset * speed}px)`;
-      });
+      if (parallaxRef.current) {
+        Array.from(parallaxRef.current.children).map((layer: Element, index) => {
+          let speed = parseFloat(layer.getAttribute("data-speed")!);
+          if (index === 0 || index === 2) {
+            speed = speed * -1;
+          }
+          (layer as HTMLElement).style.transform = `translateY(${offset * speed}px)`;
+        });
+      }
     };
     window.addEventListener("scroll", handleScroll);
 
