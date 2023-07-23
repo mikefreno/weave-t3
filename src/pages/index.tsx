5f24b31b34e0ea97b26d8046fef9f49760c3773f
MikeFreno <michaelt.freno@gmail.com>
2023-07-23 16:51:17 -0400

    minor ui change-up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,22 +5,41 @@ import Footer from "@/src/components/home/Footer";
 import LightLogo from "@/public/Logo - light.png";
 import DarkLogo from "@/public/Logo - dark.png";
 import DownloadButton from "@/src/components/home/DownloadButton";
-import { useContext, useRef } from "react";
+import { useContext, useRef, useState } from "react";
 import Head from "next/head";
 import Parallax from "@/src/components/home/Parallax";
 import Parallax_2 from "@/src/components/home/Parallax_2";
 import ThemeContext from "../components/ThemeContextProvider";
 import ModalsForSmallScreens from "../components/home/ModalsForSmallScreens";
 import StageBanner from "../components/StageBanner";
+import { Button } from "@nextui-org/react";
+import Link from "next/link";
+import { useSession } from "next-auth/react";
+import LoginModal from "../components/loginModal";
 
 const inter = Inter({ subsets: ["latin"] });
 
 const Home = () => {
   const { isDarkTheme } = useContext(ThemeContext);
   const switchRef = useRef<HTMLDivElement>(null);
+  const { data: session, status } = useSession();
+  const [showingLoginModal, setShowingLoginModal] = useState<boolean>(false);
+  const loginRef = useRef<HTMLDivElement>(null);
+  const tooltipRef = useRef<HTMLDivElement>(null);
+
+  const dependantToggleLogin = () => {
+    if (status !== "authenticated") {
+      setShowingLoginModal(true);
+    }
+  };
+
+  function loginToggle() {
+    setShowingLoginModal(!showingLoginModal);
+  }
 
   return (
     <div className="bannerBGColor w-screen">
+      {showingLoginModal ? <LoginModal onClose={loginToggle} loginRef={loginRef} tooltipRef={tooltipRef} /> : null}
       <div className={isDarkTheme ? "bgColorGradient" : "bgColorGradientLight"} id="body">
         <div id="bottomGradient" className={`${isDarkTheme ? "bottomGradient" : "bottomGradientLight"}`}>
           <Head>
@@ -39,14 +58,27 @@ const Home = () => {
               <div className="hidden pt-24 md:block">
                 <Parallax_2 isDarkTheme={isDarkTheme} />
               </div>
-              <div className="flex justify-center pt-28">
-                <Image src={isDarkTheme ? DarkLogo : LightLogo} alt="logo" width={160} height={160} />
+              <div className="relative z-0 flex justify-center pt-28">
+                <div className="slow-spin absolute z-0 mt-24">
+                  <Image src={isDarkTheme ? DarkLogo : LightLogo} alt="logo" width={240} height={240} className="z-0" />
+                </div>
               </div>
-              <h1 className="pt-24 text-center text-5xl font-semibold tracking-widest text-zinc-800 dark:text-zinc-300">
+              <h1 className="relative z-10 mt-44 bg-zinc-100 bg-opacity-30 text-center text-5xl font-semibold tracking-widest text-zinc-800 backdrop-blur-[5px] dark:text-zinc-300">
                 Group Hangouts.
                 <br /> Encrypted
               </h1>
-              <div className="-z-10 flex justify-center py-24 ">
+              <div className="-z-10 flex justify-center pb-4 pt-24">
+                <Button shadow color="gradient" auto size={"lg"} onClick={dependantToggleLogin} className="z-0">
+                  {status == "authenticated" ? (
+                    <Link href={"/app"} className="text-[#E2E2E2]">
+                      Web App
+                    </Link>
+                  ) : (
+                    "Web App"
+                  )}
+                </Button>
+              </div>
+              <div className="-z-10 flex justify-center pb-24">
                 <DownloadButton />
               </div>
               <div className="px-8 pt-8 text-center tracking-widest text-zinc-800 dark:text-zinc-300">
